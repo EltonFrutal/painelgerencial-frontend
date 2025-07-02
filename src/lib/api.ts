736b2ce980943e1e9a28361ff2713170f5a9cb20
Elton Gonçalves <eltongoncalvesdeoliveira@hotmail.com>
2025-07-01 22:13:35 -0300

    fix: ajusta baseURL do axios para utilizar NEXT_PUBLIC_API_URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api', // <-- ajuste para a porta 3001
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
 });
 
 // Adiciona automaticamente o token JWT salvo no localStorage em cada requisição
