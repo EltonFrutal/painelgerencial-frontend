cafa080b6c76c2959d7590afc7c25dbcfa64468f
Elton Gonçalves <eltongoncalvesdeoliveira@hotmail.com>
2025-08-03 18:38:03 -0300

    030825 1838h

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,20 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
+        {/* Fonte Roboto */}
         <link 
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap" 
           rel="stylesheet" 
         />
+
+        {/* Favicon para navegador */}
+        <link rel="icon" href="/favicon.ico" />
+
+        {/* Web App Manifest (para ícone em celular) */}
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/icon-192.png" />
+        <meta name="theme-color" content="#0F172A" />
+        <meta name="description" content="Painel Gerencial Interativo com Gráficos e IA" />
       </Head>
       <body>
         <Main />
